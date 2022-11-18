744465e270f2290a12ac4d6954c731d03345e23a
LIngej <lars-inge@hotmail.no>
2022-11-18 13:11:31 +0100

    Initial commit

---
+++ src/main/java/no/movies/movievaultbe/MovieVaultBeApplication.java
@@ -0,0 +1,13 @@
+package no.movies.movievaultbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieVaultBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieVaultBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/movies/movievaultbe/MovieVaultBeApplicationTests.java
@@ -0,0 +1,13 @@
+package no.movies.movievaultbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieVaultBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
